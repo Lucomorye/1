0fe8f45cd4dc8ab79b56b2ceadfea1a72174752c
Tarantin <mr.fadeyka@gmail.com>
2023-02-23 22:22:27 +0300

    Добавили ГИТИГНОР

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();// присваиваем (=) преременной (username) с строчным типом данных(string)
+                                     //Console.ReadLine(); с помощью этой комманды происходит считывание данныч
+Console.WriteLine("Привет, "); //Выводит в консоль запись
+Console.WriteLine(username);
