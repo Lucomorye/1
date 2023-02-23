77673cd5213e2e92d4e09e42c11d555c339f6a42
Tarantin <mr.fadeyka@gmail.com>
2023-02-24 01:26:01 +0300

    Example003_Sum/Program.cs

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,3 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA + numberB);
+﻿double numberA = new Random().Next(1, 10);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA / numberB);
