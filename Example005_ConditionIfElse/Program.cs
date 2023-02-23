bc0b79bff87455bef945fd58153151f5e472cbea
Tarantin <mr.fadeyka@gmail.com>
2023-02-24 02:07:12 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Youre name");
+string username = Console.ReadLine();
+if (username == "Masha")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else
+{
+    Console.Write("Привет хуйло ");
+    Console.WriteLine(username);
+}
